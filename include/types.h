ceba32a14c5c1f215875606db7631b2dc5901897
Dhruv Ajmera <dhruvajmera0@gmail.com>
2025-12-20 23:50:58 -0600

    Added personal card identification and verification.

--- include/types.h
+++ include/types.h
@@ -1,8 +1,16 @@
 #pragma once
+# include <string>
 
 namespace types{
     // All possible poker hands
-    enum class hands {HIGH_CARD=10, ONE_PAIR=20, TWO_PAIR=30, TRIPS=40, STRAIGHT=50, FLUSH=60, QUADS=70, FULL_HOUSE=80, STRAIGHT_FLUSH=90, ROYAL_FLUSH=100}; 
-    enum class cards {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, KING, QUEEN, ACE};
-    enum class suits {SPADES, CLUBS, DIAMONDS, HEARTS};
+    enum class hands {HIGH_CARD=10, ONE_PAIR=20, TWO_PAIR=30, TRIPS=40, STRAIGHT=50, FLUSH=60, QUADS=70, FULL_HOUSE=80, STRAIGHT_FLUSH=90, ROYAL_FLUSH=100};
+
+    struct card {
+        int value; // 2-14, but A is 14 and 1
+        int suit; // 1=Spades, 2=Clubs, 3=Diamonds, 4=Hearts
+
+        bool operator==(const card &other) const {
+            return value == other.value && suit == other.suit;
+        }
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,59 @@
 # include <iostream>
+# include <string>
+# include <vector>
 # include "types.h"
 
 using namespace types;
 using namespace std;
 
+card card_one;
+card card_two;
+
+vector<string> comma_split(const string& s){
+    vector<string> result;
+    string curr;
+
+    for (char c : s) {
+        if (c == ',') {
+            result.push_back(curr);
+            curr.clear();
+        }
+        else {curr.push_back(c);}
+    }
+
+    if (!curr.empty()) {result.push_back(curr);}
+
+    return result;
+}
+
+bool check_validity() {
+    if (card_one.value > 14 || card_one.value < 2) {return false;}
+    if (card_two.value > 14 || card_two.value < 2) {return false;}
+    if (card_one.suit > 4 || card_one.suit < 0) {return false;}
+    if (card_two.suit > 4 || card_two.suit < 0) {return false;}
+    if (card_one == card_two) {return false;}
+
+    return true;
+}
+
 int main() {
     types::hands my_hand = types::hands::FLUSH;
-    string s;
-    cout << "Enter the first card and suit, comma separated >>> ";
-    cin >> s;
+    string line;
+    cout << "Enter the numeric value of first card and suit, comma separated. Ace = 14, Spades = 1, Clubs = 2, Diamonds = 3, Hearts = 4. >>> ";
+    cin >> line;
+    vector<string> temp = comma_split(line);
+    int suit = stoi(temp.back());
+    temp.pop_back();
+    int val = stoi(temp.back());
+    card_one = card{val, suit};
+
+    cout << "Enter the numeric value of second card and suit, comma separated. Ace = 14, Spades = 1, Clubs = 2, Diamonds = 3, Hearts = 4. >>> ";
+    cin >> line;
+    temp = comma_split(line);
+    suit = stoi(temp.back());
+    temp.pop_back();
+    val = stoi(temp.back());
+    card_two = card{val, suit};
+
+    if (!check_validity()) {throw invalid_argument("Such a combination of cards cannot exist.");}
 }
